e4baa4b0d214183c22d8c90c01f96a3b25c02cdb
grimreaper <grimreaper@localhost.localdomain>
2015-02-16 19:02:43 -0500

    First commit. UI:A&B

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var express=require('express'),
+	app=express(),
+	server=require('http').createServer(app),
+	io=require('socket.io').listen(server);
+
+server.listen(3000);
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname+'/index.html');
+});
+
+console.log("Server Started");
+ 
