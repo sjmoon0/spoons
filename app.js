96c3d255d69c1b516c431f2fc860c913cb977bdb
grimreaper <grimreaper@localhost.localdomain>
2015-04-20 20:33:19 -0400

    Intial round game logic complete. Grab spoon removes from desktop, displays on mobile. Invalid grab disables btn for 3 sec. Desktop notifies players of loser.

--- app.js
+++ app.js
@@ -29,17 +29,48 @@ app.get('/',function(req,res){
 	}
 });
 
-/*
+
 app.use(function(req, res, next) {
   //console.log('method: '+req.method);
   //console.log('url: '+req.url);
   //console.log('path: '+req.path);
 	if(req.url!='/'){
-		if(isValidRoom(req.url.substring(1,req.url.length))){
-			//currRoom=req.url.substring(1,req.url.length);
-			//console.log("Success: "+currRoom);
+		var fileName = req.params.name;
+		if(req.url.substring(1,req.url.length)=='mobile'){
+			res.sendFile(__dirname+'/mobile.html',function(err){
+				if (err) {
+				  console.log(err);
+				  res.status(err.status).end();
+				}
+				else {
+				  console.log('Sent:', fileName);
+				}
+			});
+	  	}
+	  	else if(req.url.substring(1,req.url.length)=='desktop'){
+	  		res.sendFile(__dirname+'/index.html',function(err){
+				if (err) {
+				  console.log(err);
+				  res.status(err.status).end();
+				}
+				else {
+				  console.log('Sent:', fileName);
+				}
+			});
+	  	}
+	  	else{
+	  		res.sendFile(__dirname+'/index.html',function(err){
+				if (err) {
+				  console.log(err);
+				  res.status(err.status).end();
+				}
+				else {
+				  console.log('Sent:', fileName);
+				}
+			});
 	  	}
 	}
+	/*
   	var ua = req.headers['user-agent'].toLowerCase();
 	if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(ua)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(ua.substr(0,4))) {
 		
@@ -60,8 +91,9 @@ app.use(function(req, res, next) {
 		console.log("Desktop session"+app.mountpath);
 	}
   //next();
-});
-*/
+  */
+	});
+
 
 	var lobby={
 			name:'lobby',
@@ -69,7 +101,7 @@ app.use(function(req, res, next) {
 		}
 
 	var rooms=[];
-	var ta=[];
+	//var ta=[];
 
 	var deck=['AH','2H','3H','4H','5H','6H','7H','8H','9H','1H','JH','QH','KH',
 			  'AC','2C','3C','4C','5C','6C','7C','8C','9C','1C','JC','QC','KC',
@@ -82,6 +114,7 @@ app.use(function(req, res, next) {
 			readyToPlay:false,
 			hand: ['AS','KH','QC','JD'],
 			socketid:pid,
+			hasSpoon:false,
 			//sock:sh.s,
 			cardPile:[],
 			currCard:''
@@ -126,6 +159,8 @@ app.use(function(req, res, next) {
 			players:0,
 			viewers:[],
 			gameStarted:false,
+			goodToGrab:false,
+			remainingSpoons:0,
 			remDeck:[],
 			discardPile:[]//The last player discards hid cards into this. Then when remDeck is empty, it gets set to shuffle(discardPile)
 		}
@@ -271,7 +306,18 @@ app.use(function(req, res, next) {
 		return null;
 	}
 
-	function getPlayerOnLeft(){};
+	function isValidGrab(pl,rm){
+		console.log("Grab attempt "+pl.hand[0].charAt(0)+''+pl.hand[1].charAt(0)+''+pl.hand[2].charAt(0)+''+pl.hand[3].charAt(0));
+		return (pl.hand[0].charAt(0)==pl.hand[1].charAt(0)&&pl.hand[2].charAt(0)==pl.hand[3].charAt(0)&&pl.hand[0].charAt(0)==pl.hand[3].charAt(0))||rm.goodToGrab;
+	};
+
+	function getLoser(rm){
+		for(var i=0;i<rm.players.length;++i){
+			if(rm.players[i].hasSpoon==false){
+				return rm.players[i];
+			}
+		}
+	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~Networking~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -373,6 +419,8 @@ io.on('connection',function(socket){
 				temproom.gameStarted=true;
 				temproom.remDeck=deal(temproom,shuffle(deck));
 				io.to(info.rm).emit('all players ready',temproom);
+				temproom.remainingSpoons=temproom.players.length-1;
+				io.to(info.rm).emit('update spoons',temproom.remainingSpoons);
 				//emit update cardpile to player one. Or do something to just make the cardpile active
 			}
 		}
@@ -383,24 +431,27 @@ io.on('connection',function(socket){
 
 	socket.on('pass left',function(stuff){
 		//called when player swiped middle section left
-		//pass in the card that was passed left, username, and room name
-		//get room object from roomName
 		var rm=getRoomObject(stuff.rm);
-		//get player from username
 		var player=getPlayerFromUN(rm,stuff.un);
 		console.log(stuff.un+" passed "+stuff.card+" left");
-		if(stuff.card==''){//if (the passed card is null). We don't need to send anything to 
-			if(rm.players.indexOf(player)==0){//if(player == first player)
+		if(stuff.card==''){ 
+			if(rm.players.indexOf(player)==0){
 				if(rm.remDeck.length==0){
 					rm.remDeck=shuffle(rm.discardPile);
+					rm.discardPile=[];
 				}
-				socket.emit('display pile');
-				player.currentCard=rm.remDeck.shift();
-				socket.emit('set current card',player.currentCard);
-				//player.cardPile.push(rm.remDeck.shift());
+				if(rm.remDeck.length>0){
+					socket.emit('display pile');
+					var nextcard=rm.remDeck.shift();
+					player.currentCard=((nextcard==null)||(nextcard=='')?'':nextcard);
+
+					socket.emit('set current card',player.currentCard);
+				}
 			}
-			else if(player.cardPile.length>0){
-				player.currentCard=player.cardPile.shift();
+			else if(player.cardPile.length>=0){
+				var nextcard=player.cardPile.shift();
+				player.currentCard=((nextcard==null)||(nextcard=='')?'':nextcard);
+
 				socket.emit('set current card',player.currentCard);
 			}
 		}
@@ -408,29 +459,34 @@ io.on('connection',function(socket){
 			if(rm.players.indexOf(player)==0){
 				if(rm.remDeck.length==0){
 					rm.remDeck=shuffle(rm.discardPile);
+					rm.discardPile=[];
 				}
-				rm.players[1].cardPile.push(player.currentCard);
-				//rm.players[1].sock.emit('update pile',rm.players[1].cardPile);
-				//io.sockets.connected[rm.players[1].socketid].emit('update pile',rm.players[1].cardPile);
-				io.sockets.connected[rm.players[1].socketid].emit('display pile');
-				player.currentCard=rm.remDeck.shift();
-				socket.emit('set current card',player.currentCard);
-				//player.cardPile.push(rm.remDeck.shift());
-				socket.emit('display pile');
+				if(rm.remDeck.length>=0){
+					rm.players[1].cardPile.push(player.currentCard);
+					io.sockets.connected[rm.players[1].socketid].emit('display pile');
+					var nextcard=rm.remDeck.shift();
+					player.currentCard=((nextcard==null)||(nextcard=='')?'':nextcard);
+
+					socket.emit('set current card',player.currentCard);
+					socket.emit('display pile');
+				}
 			}
 			else{
-				if(player.cardPile.length>0){
+				if(player.cardPile.length>=0){
 					if(rm.players.indexOf(player)==(rm.players.length-1)){
 						rm.discardPile.push(player.currentCard);
 						console.log("Discarded: "+player.currentCard);
-						player.currentCard=player.cardPile.shift();
+						var nextcard=player.cardPile.shift();
+						player.currentCard=((nextcard==null)||(nextcard=='')?'':nextcard);
+
 						socket.emit('set current card',player.currentCard);
 					}
 					else{
 						rm.players[rm.players.indexOf(player)+1].cardPile.push(player.currentCard);
-						//rm.players[rm.players.indexOf(player)+1].sock.emit('update pile',rm.players[rm.players.indexOf(player)+1].cardPile);
 						io.sockets.connected[rm.players[rm.players.indexOf(player)+1].socketid].emit('display pile');
-						player.currentCard=player.cardPile.shift();
+						var nextcard=player.cardPile.shift();
+						player.currentCard=((nextcard==null)||(nextcard=='')?'':nextcard);
+
 						socket.emit('set current card',player.currentCard);
 					}
 					if(player.cardPile.length>0){
@@ -441,41 +497,62 @@ io.on('connection',function(socket){
 		}
 	});
 
-	socket.on('discard left',function(){
+	socket.on('discard',function(stuff){
 		//called when player swipes up on a card in his hand
-		//pass in the card that was discarded, the currentCard, and room name
-		//get player from socket.id
-		//get room object from roomName
-		//if(currentCard!=null)
-			//if(socket.id == last player)
-				//append discarded card to discardedPile
-				//set player.hand[indexOf(card)]=currentCard
-			//else
-				//append discarded card to player on left's cardPile
-				//send that cardPile to player on left
-				//set player.hand[indexOf(card)]=currentCard
+		var rm=getRoomObject(stuff.rm);
+		var player=getPlayerFromUN(rm,stuff.un);
+		console.log(stuff.un+" discarded card at "+stuff.slot);
+		if(player.currentCard!=''){
+			if(rm.players.indexOf(player)==(rm.players.length-1)){
+				rm.discardPile.push(player.hand[stuff.slot]);
+				player.hand[stuff.slot]=player.currentCard;
+				player.currentCard='';
 
-				//if (players cardPile !=empty)
-					//if(socket.id == first player)
-						//if(remDeck is empty)
-							//remDeck=shuffle(discardPile)
-						//set currentCard = cardPile.pop
-						//send remDeck.pop as the only element in first players cardPile
-					//set currentCard = cardPile.pop
-				//else
-					//set currentCard=null
-			//send new player.hand to this player
-
+			}
+			else{
+				rm.players[rm.players.indexOf(player)+1].cardPile.push(player.hand[stuff.slot]);
+				io.sockets.connected[rm.players[rm.players.indexOf(player)+1].socketid].emit('display pile');
+				player.hand[stuff.slot]=player.currentCard;
+				player.currentCard='';
+			}
+			socket.emit('set current card',player.currentCard);
+			socket.emit('update hand',player.hand);
+		}
 	});
 	socket.on('get hand',function(rm){
 		var cRoom=getRoomObject(rm.rm);
 		var cPlayer=getPlayerFromUN(cRoom,rm.un);
 		socket.emit('update hand',cPlayer.hand);
 	});
-	/*
-	socket.on('',function(){
-		
+	
+	socket.on('grab spoon',function(stuff){
+		var rm=getRoomObject(stuff.rm);
+		var pl=getPlayerFromUN(rm,stuff.un);
+		if(isValidGrab(pl,rm)){
+			rm.goodToGrab=true;
+			pl.hasSpoon=true;
+			if(rm.remainingSpoons>0){
+				rm.remainingSpoons--;
+				socket.emit('display spoon');
+				if(rm.remainingSpoons>=0){
+					console.log(rm.remainingSpoons+" spoons remaining");
+					io.to(rm.name).emit('update spoons',rm.remainingSpoons);
+				}
+				if(rm.remainingSpoons==0){
+					rm.goodToGrab=false;
+					console.log(getLoser(rm));
+					io.to(rm.name).emit('game over',getLoser(rm).name);
+				}
+			}
+			console.log(pl.name+" made a valid grab");
+		}
+		else{
+			console.log(pl.name+" invalid grab");
+			socket.emit('invalid grab');
+		}
 	});
+
+	/*
 	socket.on('',function(){
 		
 	});
